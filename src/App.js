6f6275d6a808572a8e48f08800958c4716fcc334
linchengzzz <1139489076@qq.com>
2020-11-19 16:36:17 +0800

    feat: add ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, Layout, message, Space, Upload, Table, Divider } from "antd";
+import { UploadOutlined } from '@ant-design/icons';
+import XLSX from "xlsx";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+import "./style.css";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            reader: new FileReader(),
+            contractData: [],
+            systemData: [],
+            columns: [],
+            dataSource: [],
+            type: null,
+        };
+    }
+
+    componentDidMount() {
+        const { reader } = this.state;
+        reader.onload = (event) => {
+            this.handleFileReader(event);
+        };
+    }
+
+    handleUploadFile = (file, type) => {
+        const { reader } = this.state;
+        this.setState({ type }, () => reader.readAsBinaryString(file));
+        return false;
+    }
+
+    handleFileReader = (event) => {
+        const { type } = this.state;
+        const data = event.target.result;
+        const workbook = XLSX.read(data, { type: 'binary' });
+        const dataSource = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+        this.setState({ [type + 'Data']: dataSource });
+        message.success('上传成功~');
+    }
+
+    handleFilter = () => {
+        const { contractData, systemData } = this.state;
+        if (contractData.length === 0) {
+            message.error('请上传合同数据~');
+            return;
+        }
+        if (systemData.length === 0) {
+            message.error('请上传系统数据~');
+            return;
+        }
+        console.log(123);
+    }
+
+
+    render() {
+        const { columns, dataSource } = this.state;
+
+        return (
+            <div className="app">
+                <Layout className="layout">
+                    <Layout.Content className="content">
+                        <Space direction="vertical" size="middle" style={{ width: '100%' }}>
+                            <div>
+                                <Space size={16}>
+                                    <Upload
+                                        beforeUpload={file => this.handleUploadFile(file, 'contract')}
+                                        showUploadList={false}
+                                    >
+                                        <Button icon={<UploadOutlined/>} type="primary">上传合同数据</Button>
+                                    </Upload>
+                                    <Upload
+                                        beforeUpload={file => this.handleUploadFile(file, 'system')}
+                                        showUploadList={false}
+                                    >
+                                        <Button icon={<UploadOutlined/>} type="primary">上传系统数据</Button>
+                                    </Upload>
+                                </Space>
+                                &nbsp;&nbsp;
+                                <Divider type="vertical" />
+                                &nbsp;&nbsp;
+                                <Button onClick={this.handleFilter}>过滤</Button>
+                            </div>
+                            <Table
+                                bordered
+                                columns={columns}
+                                dataSource={dataSource}
+                            />
+                        </Space>
+                    </Layout.Content>
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
