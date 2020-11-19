23127069cdca2530227007127948ebccf019b27f
linchengzzz <1139489076@qq.com>
2020-11-19 17:34:46 +0800

    feat: finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { Button, Layout, message, Space, Upload, Table, Divider } from "antd";
+import { Button, Layout, message, Space, Upload, Table, Divider, Alert } from "antd";
 import { UploadOutlined } from '@ant-design/icons';
 import XLSX from "xlsx";
 
 import "./style.css";
+
+const map = {
+    modal: '型号',
+}
 
 class App extends React.Component {
     constructor(props) {
@@ -11,9 +15,13 @@ class App extends React.Component {
         this.state = {
             reader: new FileReader(),
             contractData: [],
+            contractLoading: false,
             systemData: [],
+            systemLoading: false,
             columns: [],
-            dataSource: [],
+            repeatDataSource: [],
+            unRepeatDataSource: [],
+            filterLoading: false,
             type: null,
         };
     }
@@ -27,7 +35,10 @@ class App extends React.Component {
 
     handleUploadFile = (file, type) => {
         const { reader } = this.state;
-        this.setState({ type }, () => reader.readAsBinaryString(file));
+        this.setState({
+            type,
+            [type + 'Loading']: true,
+        }, () => reader.readAsBinaryString(file));
         return false;
     }
 
@@ -36,12 +47,18 @@ class App extends React.Component {
         const data = event.target.result;
         const workbook = XLSX.read(data, { type: 'binary' });
         const dataSource = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
-        this.setState({ [type + 'Data']: dataSource });
+        this.setState({
+            [type + 'Data']: dataSource,
+            [type + 'Loading']: false,
+        });
         message.success('上传成功~');
     }
 
     handleFilter = () => {
         const { contractData, systemData } = this.state;
+        const columns = [];
+        const repeatDataSource = [];
+        const unRepeatDataSource = [];
         if (contractData.length === 0) {
             message.error('请上传合同数据~');
             return;
@@ -50,42 +67,95 @@ class App extends React.Component {
             message.error('请上传系统数据~');
             return;
         }
-        console.log(123);
+        Object.keys(contractData[0]).forEach(key => {
+            columns.push({
+                title: key,
+                dataIndex: key,
+            });
+        });
+        this.setState({ filterLoading: true }, () => {
+            const len = systemData.length;
+            contractData.forEach((item, index) => {
+                const cModal = item[map.modal];
+                let i = 0;
+                for (; i < len; i++) {
+                    const sModal = systemData[i][map.modal];
+                    if (sModal.indexOf(cModal) > -1) {
+                        // 当前数据已存在系统中
+                        repeatDataSource.push({ ...item, id: index });
+                        break;
+                    }
+                }
+                if (i >= len) {
+                    // 当前数据不在系统中
+                    unRepeatDataSource.push({ ...item, id: index });
+                }
+            });
+            this.setState({ columns, repeatDataSource, unRepeatDataSource, filterLoading: false });
+        });
     }
 
-
     render() {
-        const { columns, dataSource } = this.state;
+        const {
+            columns,
+            repeatDataSource,
+            unRepeatDataSource,
+            contractLoading,
+            systemLoading,
+            filterLoading,
+        } = this.state;
 
         return (
             <div className="app">
                 <Layout className="layout">
                     <Layout.Content className="content">
-                        <Space direction="vertical" size="middle" style={{ width: '100%' }}>
+                        <Space direction="vertical" size="large" style={{ width: '100%' }}>
+                            <Alert message="温馨提示：请将要对比的列命名为 -- 型号" type="info" showIcon/>
                             <div>
-                                <Space size={16}>
+                                <Space size="middle">
                                     <Upload
                                         beforeUpload={file => this.handleUploadFile(file, 'contract')}
                                         showUploadList={false}
                                     >
-                                        <Button icon={<UploadOutlined/>} type="primary">上传合同数据</Button>
+                                        <Button
+                                            loading={contractLoading}
+                                            icon={<UploadOutlined/>}
+                                            type="primary"
+                                        >
+                                            上传合同数据
+                                        </Button>
                                     </Upload>
                                     <Upload
                                         beforeUpload={file => this.handleUploadFile(file, 'system')}
                                         showUploadList={false}
                                     >
-                                        <Button icon={<UploadOutlined/>} type="primary">上传系统数据</Button>
+                                        <Button
+                                            loading={systemLoading}
+                                            icon={<UploadOutlined/>}
+                                            type="primary"
+                                        >
+                                            上传系统数据
+                                        </Button>
                                     </Upload>
+                                    <Divider type="vertical"/>
+                                    <Button onClick={this.handleFilter}>过滤</Button>
                                 </Space>
-                                &nbsp;&nbsp;
-                                <Divider type="vertical" />
-                                &nbsp;&nbsp;
-                                <Button onClick={this.handleFilter}>过滤</Button>
                             </div>
                             <Table
+                                loading={filterLoading}
+                                title={() => "已存在系统中的数据"}
                                 bordered
+                                rowKey="id"
                                 columns={columns}
-                                dataSource={dataSource}
+                                dataSource={repeatDataSource}
+                            />
+                            <Table
+                                loading={filterLoading}
+                                title={() => "不在系统中的数据"}
+                                bordered
+                                rowKey="id"
+                                columns={columns}
+                                dataSource={unRepeatDataSource}
                             />
                         </Space>
                     </Layout.Content>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import zhCN from 'antd/lib/locale/zh_CN';
 import 'antd/dist/antd.css';
+import { ConfigProvider } from "antd";
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+    <ConfigProvider local={zhCN}>
+        <App/>
+    </ConfigProvider>,
+    document.getElementById('root')
+);
